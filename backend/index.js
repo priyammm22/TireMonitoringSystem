f8bc1fc957c9c6889c1c434f6e444fbb3017115d
priyammm22 <128636575+priyammm22@users.noreply.github.com>
2024-11-18 16:45:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const { connectDB } = require("./db/db");
-const tireRoutes = require("./routes/tireRoutes.js"); // Import tire routes
+const tireRoutes = require("./routes/tireRoutes.js"); 
 
 const app = express();
 app.use(express.json());
 
-// Connect to MongoDB
+
 connectDB();
 
-// Use tire routes
-app.use("/tire", tireRoutes); // All routes defined in tireRoutes will be prefixed with "/tire"
 
-// Start the server
+app.use("/tire", tireRoutes); 
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
